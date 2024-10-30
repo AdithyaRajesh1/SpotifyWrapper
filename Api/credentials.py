d02e39689d56166680b91240b1f855d0731f573f
Adithya Rajesh <adithya.rk06@gmail.com>
2024-10-30 13:23:51 -0400

    rebase main onto megha

--- Api/credentials.py
+++ Api/credentials.py
@@ -1,3 +1,3 @@
-CLIENT_ID = "f657a8dcbb3242e580819d93dc1380c8"
-CLIENT_SECRET = "e69f8a6ee6e34266b9ab17cac81c0bb8"
-REDIRECT_URI = "http://localhost:8000/spotify/redirect/"
+CLIENT_ID = "9f913852b772462792d678d78fcadfa5"
+CLIENT_SECRET = "d269676339f24a2cab18cb694b2fd9da"
+REDIRECT_URI = "http://localhost:5173/callback"
