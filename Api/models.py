00a044be6a4623198c576a067c591a867941b556
Adithya Rajesh <adithya.rk06@gmail.com>
2024-10-30 16:02:20 -0400

    full user auth + redirect

---
+++ Api/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here
+
+class Token(models.Model):
+    access_token = models.CharField(max_length=500)
+    refresh_token = models.CharField(max_length=500)
+    expires_in = models.DateTimeField()
+    user = models.CharField(unique = True, max_length=50)
+    token_type = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
