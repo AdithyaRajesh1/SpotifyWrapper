42d92165efdea600dd82a031f541eb28f1ee9169
eshan1jaffar <80078879+eshan1jaffar@users.noreply.github.com>
2024-11-04 16:02:31 -0500

    fixed random errors

--- Api/urls.py
+++ Api/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from Api.views import spotify_redirect, CheckAuthentication, CurrentSong
+from Api.views import spotify_redirect, CheckAuthentication, CurrentSong, Authentication, home
 
 urlpatterns = [
+
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('redirect/', spotify_redirect),
-    path('check-auth/', CheckAuthentication.as_view()),
+    path('redirect/', spotify_redirect, name='redirect'),
+    path('check-auth/', Authentication.as_view(), name='check-auth'),
     path('current-song/', CurrentSong.as_view()),
 ]
