f2a288a3f6f47a2652d0c7dd238359332d682ee4
eshan1jaffar <80078879+eshan1jaffar@users.noreply.github.com>
2024-11-04 16:03:06 -0500

    fixed random errors

--- Api/credentials.py
+++ Api/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "9f913852b772462792d678d78fcadfa5"
 CLIENT_SECRET = "d269676339f24a2cab18cb694b2fd9da"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/current-song"
+REDIRECT_URI = "http://localhost:8000/spotify/redirect/"
