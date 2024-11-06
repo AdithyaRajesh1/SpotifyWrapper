8720a7f258e50fa2e8787d360063dcde5860a80b
aishanibal <aishani.bal5@gmail.com>
2024-11-06 15:48:27 -0500

    halfway through getting 2023 wrapped

--- Api/urls.py
+++ Api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from Api.views import spotify_redirect, CheckAuthentication, CurrentSong, Authentication, home, TopSongs2023
+from Api.views import spotify_redirect, CheckAuthentication, CurrentSong, Authentication, home, TopSongs, \
+    SpotifyWrappedView
 
 urlpatterns = [
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('redirect/', spotify_redirect, name='redirect'),
     path('check-auth/', Authentication.as_view(), name='check-auth'),
     path('current-song/', CurrentSong.as_view()),
-    path('wrapped/', TopSongs2023.as_view())
+    path('top/', TopSongs.as_view()),
+    path('wrapped/', SpotifyWrappedView.as_view())
 ]
