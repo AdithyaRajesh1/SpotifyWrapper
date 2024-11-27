4e5978ae4e94e3101417a03067af6765238651b4
Adithya Rajesh <adithya.rk06@gmail.com>
2024-11-27 18:56:11 -0500

    half done

--- Api/views.py
+++ Api/views.py
@@ -1,3 +1,4 @@
+#ADITHYAVERSION
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework.response import Response
